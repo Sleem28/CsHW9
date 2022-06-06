3122b469a5d3471ff9a54fa403c190f565a1d208
Kiryl Pashkin <sleembelbox@gmail.com>
2022-06-06 13:27:36 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Показать натуральные числа от M до N, N и M заданы
+//----------------------------------------------------------------------------------------------------------------------+
+void PrintNumbers(int m, int n)
+{
+    Console.Write(m + " ");
+    if(m == n) return;
+    m++;
+    PrintNumbers(m,n);
+}
+//----------------------------------------------------------------------------------------------------------------------+
+bool numIsNatural = false;
+int M = 0, N = 0;
+while(!numIsNatural)
+{
+    Console.Write("Введите число М: ");
+    M = int.Parse(Console.ReadLine()?? "");
+    if(M>0) 
+        numIsNatural = true;
+    else
+        Console.WriteLine("Число М должно быть натуральным!!! Введите его еще раз.");
+}
+
+Console.Write("Введите число N: ");
+N = int.Parse(Console.ReadLine()?? "");
+
+PrintNumbers(M,N);
