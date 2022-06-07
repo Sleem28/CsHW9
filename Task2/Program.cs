c4cda56b14d8bd3e2ceb0168890b2b3d1fb0a9b9
Kiryl Pashkin <sleembelbox@gmail.com>
2022-06-07 12:51:29 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+int SumElement(int m, int n)
+{
+    int sum = m;
+    if(m == n) return(sum);
+    m++;
+    return(sum + SumElement(m,n));
+}
+
+//---------------------------------------------------Main----------------------------------------------------------------+
+Console.Clear();
+Console.Write("Введите число М: ");
+int M = int.Parse(Console.ReadLine()?? "");
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()?? "");
+
+Console.WriteLine($"Сумма элементов от {M} до {N} равна {SumElement(M,N)}.");
+
